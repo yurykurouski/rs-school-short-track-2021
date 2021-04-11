dd3a19ca58d18c1201243f617af7b769fc8c4dfd
Yury <ymorpheus@gmail.com>
2021-04-11 17:42:11 +0300

    feat: add 14-queue solution

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,62 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.queue = {};
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    let count = 0;
+    let copy = { ...this.queue };
+
+    while (copy) {
+      count++;
+      copy = copy.next;
+    }
+
+    return count;
+  }
+
+  enqueue(element) {
+    const nodeList = new ListNode(element);
+
+    if (!this.queue.value) {
+      this.queue = nodeList;
+      return;
+    }
+
+    const arrayed = [];
+    let copy = { ...this.queue };
+
+    while (copy) {
+      arrayed.unshift(copy.value);
+      copy = copy.next;
+    }
+
+    arrayed.unshift(element);
+
+    this.queue = arrayed.reduce((acc, el) => {
+      if (acc) {
+        const listNode = { ...acc, value: el, next: acc };
+        return listNode;
+      }
+      const listNode = { value: el, next: null };
+      return listNode;
+    }, null);
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const top = this.queue.value;
+
+    if (this.queue.next) {
+      this.queue.value = this.queue.next.value;
+      this.queue.next = this.queue.next.next;
+
+      return top;
+    }
+    this.queue = {};
+
+    return top;
   }
 }
 
